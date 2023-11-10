49dfa3d941af8cfbecddc7bd70790318969d45f5
andreypats <andrey.it.work@gmail.com>
2023-11-11 00:06:28 +0300

    Add applyMiddleware

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,15 @@ import { Menu } from '@mui/icons-material';
 import {
     addTodolistAC,
     changeTodolistFilterAC,
-    changeTodolistTitleAC,
+    changeTodolistTitleAC, fetchTodolistsThunk,
     FilterValuesType,
-    removeTodolistAC, setTodolistsAC,
+    removeTodolistAC,
     TodolistDomainType
 } from './state/todolists-reducer'
 import { addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC } from './state/tasks-reducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppRootStateType } from './state/store';
-import {TaskStatuses, TaskType, todolistsAPI} from './api/todolists-api'
+import {TaskStatuses, TaskType} from './api/todolists-api'
 
 
 export type TasksStateType = {
@@ -37,9 +37,7 @@ function App() {
     const dispatch = useDispatch();
 
     useEffect(()=>{
-        todolistsAPI.getTodolists().then((res)=>{
-            dispatch(setTodolistsAC(res.data))
-        })
+        dispatch(fetchTodolistsThunk)
     },[])
 
     const removeTask = useCallback(function (id: string, todolistId: string) {
